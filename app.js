c2074c3e0c8d0b71ae29c76c6cfa50b3bfe62dab
Dimitri Pirogov <73475112+DimPirogov@users.noreply.github.com>
2022-09-08 16:17:37 +0200

    last pictures and slide

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+// show navbar
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+	navbarLinks.classList.toggle("active");
+});
+
+//Slideshow functionality
+function showSlides() {
+	let i;
+
+	let slides = document.getElementsByClassName("mySlides");
+
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+
+	slideIndex++;
+
+	if (slideIndex > slides.length) {
+		slideIndex = 1;
+	}
+
+	slides[slideIndex - 1].style.display = "block";
+
+	setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
